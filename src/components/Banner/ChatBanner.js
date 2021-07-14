509070ca5a69a07e445de75fe2aadfbb323b8652
redheet <naufalzidane32@gmail.com>
2021-07-14 12:39:39 +0700

    update time slider

--- src/components/Banner/ChatBanner.js
+++ src/components/Banner/ChatBanner.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import image1 from '../../img/background/bg_1.jpg';
 import image2 from '../../img/background/bg_2.jpg';
@@ -6,41 +6,29 @@ import image3 from '../../img/background/bg_3.jpg';
 
 const ChatBanner = () => {
   const mystyle = {
-    color: "white",
+    color: 'white',
   };
   return (
     <Carousel fade={true}>
-  <Carousel.Item interval={1000}>
-    <img
-      className="d-block w-100 mr-3"
-      src={image1}
-      alt="ISIF"
-    />
-    <Carousel.Caption>
-      <h3 style={mystyle}>International Science And Invention Fair</h3>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item interval={500}>
-    <img
-      className="d-block w-100"
-      src={image2}
-      alt="IRTC"
-    />
-    <Carousel.Caption>
-      <h3 style={mystyle}>International Science And Invention Fair</h3>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item interval={200}>
-    <img
-      className="d-block w-100"
-      src={image3}
-      alt="NRTC"
-    />
-    <Carousel.Caption>
-      <h3 style={mystyle}>International Science And Invention Fair</h3>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
+      <Carousel.Item interval={2000}>
+        <img className="d-block w-100 mr-3" src={image1} alt="ISIF" />
+        <Carousel.Caption>
+          <h3 style={mystyle}>International Science And Invention Fair</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+        <img className="d-block w-100" src={image2} alt="IRTC" />
+        <Carousel.Caption>
+          <h3 style={mystyle}>International Science And Invention Fair</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+        <img className="d-block w-100" src={image3} alt="NRTC" />
+        <Carousel.Caption>
+          <h3 style={mystyle}>International Science And Invention Fair</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 };
 
