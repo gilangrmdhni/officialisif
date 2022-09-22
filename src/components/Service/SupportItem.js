811d90e5c0c4a95f7a884b806d2c7448ce4190a3
naufalziidane_ <41244267+redheet@users.noreply.github.com>
2022-09-22 14:32:07 +0700

    update

--- src/components/Service/SupportItem.js
+++ src/components/Service/SupportItem.js
@@ -9,14 +9,14 @@ const SupportItem = () => {
           <div className="support_item">
             <img src={require('../../img/new-home/guide.png')} alt="" />
             <h4>Guide Book</h4>
-            <a href="https://drive.google.com/drive/folders/195oPkTAYTc3JriGHXibKFtN0uUJCB_Er?usp=sharing" className="software_banner_btn btn_submit f_size_15 f_500">
+            <a href="https://drive.google.com/file/d/1u3bkSMql1vXmfDZymlONYTFzrW9wZwg6/view" className="software_banner_btn btn_submit f_size_15 f_500">
               Download
             </a>
           </div>
           <div className="support_item">
             <img src={require('../../img/new-home/extended.png')} alt="" />
             <h4>Example Extended Abstract</h4>
-            <a href="https://drive.google.com/drive/folders/1y0MEwFp17T6lxakILhS4cVHoZBq9t4fE?usp=sharing" className="software_banner_btn btn_submit f_size_15 f_500">
+            <a href="https://drive.google.com/file/d/1u3bkSMql1vXmfDZymlONYTFzrW9wZwg6/edit?disco=AAAAghNuBRY" className="software_banner_btn btn_submit f_size_15 f_500">
               Download
             </a>
           </div>
