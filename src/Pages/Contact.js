4dd985a6a7db49857f929e8d70ed0f8c961a1578
iysaofficial <110337759+iysaofficial@users.noreply.github.com>
2022-10-06 14:53:03 +0700

    remove core features

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -4,7 +4,7 @@ import Breadcrumb from '../components/Breadcrumb';
 // import Contacts from '../components/Contacts';
 import Footer from '../components/Footer/Footer';
 import FooterData from '../components/Footer/FooterData';
-import CoreFeatures from '../components/Features/CoreFeatures';
+// import CoreFeatures from '../components/Features/CoreFeatures';
 import MarketingGetstarted from '../components/MarketingGetstarted';
 import EventLocation from '../components/EventLocation';
 
@@ -14,7 +14,7 @@ const About = () => {
       <CustomNavbar slogo="sticky_logo" mClass="menu_four" nClass="w_menu ml-auto mr-auto" />
       <Breadcrumb breadcrumbClass="breadcrumb_area" imgName="breadcrumb/call_me.jpg" Ptitle="Contac Us" Pdescription="Please feel free to contact us if you need other assistance" />
       {/* <Contacts /> */}
-      <CoreFeatures />
+      {/* <CoreFeatures /> */}
       <MarketingGetstarted />
       <EventLocation />
       <Footer FooterData={FooterData} />
