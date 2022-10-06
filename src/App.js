f7166a6e8393c4fb0018a251c64a0026a6cef5be
naufalziidane_ <41244267+redheet@users.noreply.github.com>
2022-10-06 16:56:19 +0700

    coments not need

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 /*------ Pages-----*/
 import { Home } from './Pages/Home';
-import DigitalMarketing from './Pages/Digital-marketing';
-import PaymentProcessing from './Pages/Payment-processing';
-import HRManagement from './Pages/HR-Management';
-import Startup from './Pages/Startup';
-import HomeCRM from './Pages/Home-CRM';
+// import DigitalMarketing from './Pages/Digital-marketing';
+// import PaymentProcessing from './Pages/Payment-processing';
+// import HRManagement from './Pages/HR-Management';
+// import Startup from './Pages/Startup';
+// import HomeCRM from './Pages/Home-CRM';
 import About from './Pages/About';
-import Service from './Pages/Service';
+// import Service from './Pages/Service';
 import Process from './Pages/Process';
-import Team from './Pages/Team';
+// import Team from './Pages/Team';
 import Portfolio2col from './Pages/Portfolio-2col';
 import GalleryF from './Pages/GalleryF';
 import Photo from './Pages/Photo';
@@ -19,23 +19,23 @@ import Bloglist from './Pages/Bloglist';
 import BlogSingle from './Pages/BlogSingle';
 import Contact from './Pages/Contact';
 import ScrollToTopRoute from './ScrollToTopRoute';
-import Landing from './Pages/Landing';
-import homesupport from './Pages/home-support';
-import homeERP from './Pages/Home-ERP';
-import homeHosting from './Pages/HomeHosting';
-import homeSecurity from './Pages/HomeSecurity';
-import homeSoftwareDark from './Pages/Home-software-dark';
-import HomeAppShowcase from './Pages/Home-app-showcase';
-import HomeCloud from './Pages/Home-cloud';
-import HomeTracking from './Pages/Home-tracking';
-import Events from './Pages/Events';
-import HomeChat from './Pages/Home-chat';
-import Price from './Pages/Price';
-import Faq from './Pages/Faq';
-import ServiceDetails from './Pages/ServiceDetails';
-import SignIn from './Pages/SignIn';
-import SignUp from './Pages/SignUp';
-import BlogGridPage from './Pages/BlogGridPage';
+// import Landing from './Pages/Landing';
+// import homesupport from './Pages/home-support';
+// import homeERP from './Pages/Home-ERP';
+// import homeHosting from './Pages/HomeHosting';
+// import homeSecurity from './Pages/HomeSecurity';
+// import homeSoftwareDark from './Pages/Home-software-dark';
+// import HomeAppShowcase from './Pages/Home-app-showcase';
+// import HomeCloud from './Pages/Home-cloud';
+// import HomeTracking from './Pages/Home-tracking';
+// import Events from './Pages/Events';
+// import HomeChat from './Pages/Home-chat';
+// import Price from './Pages/Price';
+// import Faq from './Pages/Faq';
+// import ServiceDetails from './Pages/ServiceDetails';
+// import SignIn from './Pages/SignIn';
+// import SignUp from './Pages/SignUp';
+// import BlogGridPage from './Pages/BlogGridPage';
 import NotFound from './Pages/404';
 
 class App extends Component {
@@ -47,7 +47,7 @@ class App extends Component {
       <Router>
         <Switch>
           <ScrollToTopRoute exact={true} path={'/'} component={Home} />
-          <ScrollToTopRoute
+          {/* <ScrollToTopRoute
             path="/Digital-marketing"
             component={DigitalMarketing}
           />
@@ -57,11 +57,11 @@ class App extends Component {
           />
           <ScrollToTopRoute path="/HR-Management" component={HRManagement} />
           <ScrollToTopRoute path="/Startup" component={Startup} />
-          <ScrollToTopRoute path="/Home-CRM" component={HomeCRM} />
+          <ScrollToTopRoute path="/Home-CRM" component={HomeCRM} /> */}
           <ScrollToTopRoute path="/About" component={About} />
-          <ScrollToTopRoute path="/Service" component={Service} />
+          {/* <ScrollToTopRoute path="/Service" component={Service} /> */}
           <ScrollToTopRoute path="/Process" component={Process} />
-          <ScrollToTopRoute path="/Team" component={Team} />
+          {/* <ScrollToTopRoute path="/Team" component={Team} /> */}
           <ScrollToTopRoute path="/Portfolio-2col" component={Portfolio2col} />
           <ScrollToTopRoute path="/GalleryF" component={GalleryF} />
           <ScrollToTopRoute path="/Photo" component={Photo} />
@@ -71,14 +71,14 @@ class App extends Component {
           />
           <ScrollToTopRoute path="/Bloglist" component={Bloglist} />
           <ScrollToTopRoute path="/BlogSingle" component={BlogSingle} />
-          <ScrollToTopRoute path="/BlogGridPage" component={BlogGridPage} />
+          {/* <ScrollToTopRoute path="/BlogGridPage" component={BlogGridPage} /> */}
           <ScrollToTopRoute path="/Contact" component={Contact} />
-          <ScrollToTopRoute path="/Landing" component={Landing} />
-          <ScrollToTopRoute path="/home-support" component={homesupport} />
-          <ScrollToTopRoute path="/HomeHosting" component={homeHosting} />
-          <ScrollToTopRoute path="/Home-ERP" component={homeERP} />
-          <ScrollToTopRoute path="/HomeSecurity" component={homeSecurity} />
-          <ScrollToTopRoute
+          {/* <ScrollToTopRoute path="/Landing" component={Landing} /> */}
+          {/* <ScrollToTopRoute path="/home-support" component={homesupport} /> */}
+          {/* <ScrollToTopRoute path="/HomeHosting" component={homeHosting} /> */}
+          {/* <ScrollToTopRoute path="/Home-ERP" component={homeERP} /> */}
+          {/* <ScrollToTopRoute path="/HomeSecurity" component={homeSecurity} /> */}
+          {/* <ScrollToTopRoute
             path="/HomeAppShowcase"
             component={HomeAppShowcase}
           />
@@ -94,7 +94,7 @@ class App extends Component {
           <ScrollToTopRoute path="/Faq" component={Faq} />
           <ScrollToTopRoute path="/ServiceDetails" component={ServiceDetails} />
           <ScrollToTopRoute path="/SignIn" component={SignIn} />
-          <ScrollToTopRoute path="/SignUp" component={SignUp} />
+          <ScrollToTopRoute path="/SignUp" component={SignUp} /> */}
           <ScrollToTopRoute component={NotFound} />
         </Switch>
       </Router>
