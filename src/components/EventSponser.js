343bc6adb2e0653129e4ee776c22b272c30ea5d9
naufalziidane_ <41244267+redheet@users.noreply.github.com>
2022-10-06 12:49:21 +0700

    add by text

--- src/components/EventSponser.js
+++ src/components/EventSponser.js
@@ -14,7 +14,7 @@ const EventSponser = () => {
           <div className="event_sponser_item">
             <div className="row">
               <div className="col-lg-3 col-md-4">
-                <h4 className="h_head sponser_title">Organized</h4>
+                <h4 className="h_head sponser_title">Organized By</h4>
               </div>
               <div className="col-lg-9 col-md-8">
                 <div className="row">
