0b37f7be85b9cd3b8bbf04be03f207155a0f5d9a
naufalziidane_ <41244267+redheet@users.noreply.github.com>
2022-10-06 17:15:14 +0700

    change image breadcrum

--- src/Pages/BlogGridPage.js
+++ src/Pages/BlogGridPage.js
@@ -2,16 +2,16 @@ import React from 'react';
 import CustomNavbar from '../components/CustomNavbar';
 import Breadcrumb from '../components/Breadcrumb';
 import BlogGrid from '../components/Blog/BlogGrid';
-import FooterTwo from '../components/Footer/FooterTwo';
+import Footer from '../components/Footer/Footer';
 import FooterData from '../components/Footer/FooterData';
 
 const BlogGridPage = () => {
     return(
         <div className="body_wrapper">
             <CustomNavbar slogo="sticky_logo" mClass="menu_four" nClass="w_menu ml-auto mr-auto"/>
-            <Breadcrumb breadcrumbClass="breadcrumb_area" imgName="breadcrumb/banner_bg.png" Ptitle="Blog Grid" Pdescription="Why I say old chap that is spiffing off his nut arse pear shaped plastered Jeffrey bodge barney some dodgy.!!"/>
+            <Breadcrumb breadcrumbClass="breadcrumb_area" imgName="breadcrumb/isif.jpg" Ptitle="Blog Grid" Pdescription="Blog Isif "/>
             <BlogGrid/>
-            <FooterTwo FooterData={FooterData}/>
+            <Footer FooterData={FooterData}/>
         </div>
     )
 }
