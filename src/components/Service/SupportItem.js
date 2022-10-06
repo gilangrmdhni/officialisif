19130d821511b524c841e5a15dc00951589a224b
naufalziidane_ <41244267+redheet@users.noreply.github.com>
2022-10-06 12:51:31 +0700

    change link guide book and abstract

--- src/components/Service/SupportItem.js
+++ src/components/Service/SupportItem.js
@@ -9,14 +9,14 @@ const SupportItem = () => {
           <div className="support_item">
             <img src={require('../../img/new-home/guide.png')} alt="" />
             <h4>Guide Book</h4>
-            <a href="https://drive.google.com/file/d/1u3bkSMql1vXmfDZymlONYTFzrW9wZwg6/view" className="software_banner_btn btn_submit f_size_15 f_500">
+            <a href="https://drive.google.com/file/d/1ilWuOn5-sOq2yY7S-vD5WyEc53S1RS4N/view" className="software_banner_btn btn_submit f_size_15 f_500">
               Download
             </a>
           </div>
           <div className="support_item">
             <img src={require('../../img/new-home/extended.png')} alt="" />
             <h4>Example Extended Abstract</h4>
-            <a href="https://drive.google.com/file/d/1u3bkSMql1vXmfDZymlONYTFzrW9wZwg6/edit?disco=AAAAghNuBRY" className="software_banner_btn btn_submit f_size_15 f_500">
+            <a href="https://drive.google.com/file/d/1ilWuOn5-sOq2yY7S-vD5WyEc53S1RS4N/edit?disco=AAAAgqC82Cc" className="software_banner_btn btn_submit f_size_15 f_500">
               Download
             </a>
           </div>
