43e38a5ee5616c09c99f45afa7b6a22b066061e7
naufalziidane_ <41244267+redheet@users.noreply.github.com>
2022-10-07 14:38:55 +0700

    remove page curent

--- src/components/Blog/Bloglists.js
+++ src/components/Blog/Bloglists.js
@@ -10,8 +10,8 @@ const Bloglists =()=>{
                     <div className="col-lg-8">
                         <Bloglistitem ServiceData={ServiceData}/>
                         <ul className="list-unstyled page-numbers shop_page_number text-left mt_30">
-                            <li><span aria-current="page" className="page-numbers current">1</span></li>
-                            <li><a className="page-numbers" href=".#">2</a></li>
+                            {/* <li><span aria-current="page" className="page-numbers current">1</span></li>
+                            <li><a className="page-numbers" href=".#">2</a></li> */}
                             {/* <li><a className="next page-numbers" href=".#"><i className="ti-arrow-right"></i></a></li> */}
                         </ul>
                     </div>
