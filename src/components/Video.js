7d2bb14f5b245b4787993edad88597409519e532
iysaofficial <110337759+iysaofficial@users.noreply.github.com>
2022-10-10 11:11:40 +0700

    update highlight video to after event video

--- src/components/Video.js
+++ src/components/Video.js
@@ -6,7 +6,7 @@ const video = () => {
     <section className="video_area bg_color sec_pad">
       <div className="container">
         <Sectitle
-          Title="ISIF HIGHLIGHT"
+          Title="AFTER EVENT"
           TitleP="This event was joined by multiple countries such as Germany, Turkey, Turkmenistan, Thailand, Philippines, Vietnam, Malaysia, and of course, Indonesia. It was a melting pot of diversity and creativity, place where we can express ourselves."
           tClass="t_color3"
           sClass="sec_title text-center mb_70"
@@ -14,7 +14,7 @@ const video = () => {
         <div className="video_content">
           <div className="video_info">
             <div className="ovarlay_color"></div>
-            <a className="popup-youtube video_icon" href="https://youtu.be/6DX7ZF_jqS8">
+            <a className="popup-youtube video_icon" href="https://www.youtube.com/watch?v=YHQVYtiiCC0">
               <i className="arrow_triangle-right"></i>
             </a>
             <h2>ISIF</h2>
