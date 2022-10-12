0be64bdceadaf2acf848ca4de160e3165617e68e
iysaofficial <110337759+iysaofficial@users.noreply.github.com>
2022-10-12 10:33:27 +0700

    update blog 3

--- src/Pages/Iysa-collaboration.js
+++ src/Pages/Iysa-collaboration.js
@@ -11,6 +11,7 @@ const Iysacollab = () => {
             <CustomNavbar slogo="sticky_logo" mClass="menu_four" nClass="w_menu ml-auto mr-auto"/>
             <Breadcrumb breadcrumbClass="breadcrumb_area" imgName="breadcrumb/bg.jpg" Ptitle="IYSA Collaboration" Pdescription="Home - Blog"/>
             <IysaCollab/>
+            <IysaCollab2/>
             <Footer FooterData={FooterData}/>
         </div>
     )
