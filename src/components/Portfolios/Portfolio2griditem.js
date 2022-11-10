cf99a5a5bdbc09927d04f5f9f966527c051ae5dd
redheet <naufalzidane32@gmail.com>
2022-11-11 01:23:44 +0700

    remove icon plus

--- src/components/Portfolios/Portfolio2griditem.js
+++ src/components/Portfolios/Portfolio2griditem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 class Portfolio2griditem extends Component {
   render() {
@@ -9,11 +9,14 @@ class Portfolio2griditem extends Component {
         <div className="portfolio_img">
           <img src={require('../../img/portfolio/' + pImg)} alt="" />
           <div className="hover_content">
-            <a href=".#" className="img_popup leaf">
+            {/* <a href=".#" className="img_popup leaf">
               <i className="ti-plus"></i>
-            </a>
+            </a> */}
             <div className="portfolio-description leaf">
-              <a href="https://drive.google.com/drive/folders/1g-dfsRAEqqLAVmeO3uIxtUyjE8C0UB_s?usp=sharing" className="portfolio-title">
+              <a
+                href="https://drive.google.com/drive/folders/1g-dfsRAEqqLAVmeO3uIxtUyjE8C0UB_s?usp=sharing"
+                className="portfolio-title"
+              >
                 <h3 className="f_500 f_size_20 f_p">{ptitle}</h3>
               </a>
               <div className="links">
